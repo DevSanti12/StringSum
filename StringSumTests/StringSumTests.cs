7402008db2b283b442daafe0b833d0d7a1c44cd1
Santiago Leyva <99155189+DevSanti12@users.noreply.github.com>
2025-05-25 19:29:34 -0500

    Added basic string operation tests

--- StringSumTests/StringSumTests.cs
+++ StringSumTests/StringSumTests.cs
@@ -5,6 +5,18 @@ namespace StringSumTests
 {
     public class StringSumTests
     {
+        [Fact]
+        public void StringSum_EmptyStrings_ReturnsZero()
+        {
+            var result = StringSumUtility.StringSum("", "");
+            Assert.Equal("0", result);
+        }
 
+        [Fact]
+        public void StringSum_SmallNumbers_ReturnsSum()
+        {
+            var result = StringSumUtility.StringSum("2", "3");
+            Assert.Equal("5", result);
+        }
     }
 }
