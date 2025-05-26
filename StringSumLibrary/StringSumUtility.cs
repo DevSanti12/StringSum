ba5e4a64b535cfb74c787f7bbc83d6e9ef2a1900
Santiago Leyva <99155189+DevSanti12@users.noreply.github.com>
2025-05-25 19:34:27 -0500

    Added StringSum logic in StringSumLibrary

--- StringSumLibrary/StringSumUtility.cs
+++ StringSumLibrary/StringSumUtility.cs
@@ -1,7 +1,20 @@
-﻿namespace StringSumLibrary
+﻿
+namespace StringSumLibrary
 {
     public static class StringSumUtility
     {
+        public static string StringSum(string number1, string number2)
+        {
+            int TryParseOrZero(string input)
+            {
+                return int.TryParse(input, out var result) ? result : 0;
+            }
 
+            int num1 = TryParseOrZero(number1);
+            int num2 = TryParseOrZero(number2);
+
+            return (num1 + num2).ToString();
+
+        }
     }
 }
