105042a7f0414778a08a7548daf926da12077e27
Santiago Leyva <99155189+DevSanti12@users.noreply.github.com>
2025-05-25 19:42:34 -0500

    Added 3 more tests, null check test failing

--- StringSumTests/StringSumTests.cs
+++ StringSumTests/StringSumTests.cs
@@ -18,5 +18,27 @@ namespace StringSumTests
             var result = StringSumUtility.StringSum("2", "3");
             Assert.Equal("5", result);
         }
+
+        [Fact]
+        public void StringSum_InvalidInputs()
+        {
+            var result = StringSumUtility.StringSum("abc", "5");
+            Assert.Equal("5", result);
+        }
+
+        [Fact]
+        public void StringSum_ZeroAndLargeNumbers_CorrectSumReturned()
+        {
+            var result = StringSumUtility.StringSum("0", "5000");
+            Assert.Equal("5000", result);
+        }
+
+        [Fact]
+        public void StringSum_NullInput_ReturnsNullException()
+        {
+            string num1 = null;
+            string num2 = null;
+            Assert.Throws<ArgumentNullException>(() => StringSumUtility.StringSum(num1, num2));
+        }
     }
 }
